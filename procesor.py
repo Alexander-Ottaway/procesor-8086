db1f85b2b7b11b5e89b6da4692b9bc4f6d353301
Alexander-Ottaway <alexander.ottaway@microsoft.wsei.edu.pl>
2023-10-29 13:06:48 +0100

    XCHG

--- procesor.py
+++ procesor.py
@@ -51,38 +51,67 @@ def MOV(registers):
     os.system('color 01')
     display_registers()
     selected_register = ""
-    to_home = False
-    
+
     print("\nWelcome to MOV menu. Here you can copy value from one register into the other\n")
-    
-    
-    while to_home == False:
         
-        while selected_register not in registers:
-            selected_register = input("Select a register to copy value FROM: ").lower()
-            if selected_register in registers:
-                memory = registers[selected_register]
-            else:
-                print("Not a proper input, try again...")
-        
-        selected_register = ""
+    while selected_register not in registers:
+        selected_register = input("Select a register to copy value FROM: ").lower()
+        if selected_register in registers:
+            memory = registers[selected_register]
+        else:
+            print("Not a proper input, try again...")
+    
+    selected_register = ""
 
-        while selected_register not in registers:
-            selected_register = input("Select a register to copy value TO: ").lower()
-            if selected_register in registers:
-                registers[selected_register] = memory
-                del memory
-            else:
-                print("Not a proper input, try again...")
+    while selected_register not in registers:
+        selected_register = input("Select a register to copy value TO: ").lower()
+        if selected_register in registers:
+            registers[selected_register] = memory
+            del memory
+        else:
+            print("Not a proper input, try again...")
 
-        os.system('cls')
-        print("Moving value...\n")
-        display_registers()
-        time.sleep(1.5)
-        break
+    os.system('cls')
+    print("Moving value...\n")
+    display_registers()
+    time.sleep(1.5)
+
     return registers
     
+# funkcja opcji #3 Wymiana wartości rejestrów
+def XCHG(registers):
+    os.system('cls')
+    os.system('color 0D')
+    display_registers()
+    selected_register1 = ""
+    selected_register2 = ""
     
+    print("\nWelcome to XCHG menu. Here you can switch the values between two registers\n")
+        
+    while selected_register1 not in registers:
+        selected_register1 = input("Select a register number one: ").lower()
+        if selected_register1 in registers:
+            memory = registers[selected_register1]
+        else:
+            print("Not a proper input, try again...")
+    
+    selected_register2 = ""
+
+    while selected_register2 not in registers:
+        selected_register2 = input("Select a register number two: ").lower()
+        if selected_register2 in registers:
+            registers[selected_register1] = registers[selected_register2]
+            registers[selected_register2] = memory
+            del memory
+        else:
+            print("Not a proper input, try again...")
+
+    os.system('cls')
+    print("Exchanging values...\n")
+    display_registers()
+    time.sleep(1.5)
+
+    return registers
     
      
 
@@ -126,6 +155,9 @@ while selected_option != "-1":
         case '2':
             os.system('cls')
             registers = MOV(registers)
+        case '3':
+            os.system('cls')
+            registers = XCHG(registers)
         case '-1':
             break
         case default :
